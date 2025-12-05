191db5e68dcda3c09a6e178413d025c39bead888
SeungYeop-Han <ammezkhan@gmail.com>
2025-12-05 11:30:51 +0900

    first commit

---
+++ src/main/java/com/gorogoro/followandlike/GorogoroFollowAndLikeApplication.java
@@ -0,0 +1,11 @@
+package com.gorogoro.followandlike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorogoroFollowAndLikeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GorogoroFollowAndLikeApplication.class, args);
+    }
+}
---
+++ src/test/java/com/gorogoro/followandlike/GorogoroFollowAndLikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorogoro.followandlike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GorogoroFollowAndLikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

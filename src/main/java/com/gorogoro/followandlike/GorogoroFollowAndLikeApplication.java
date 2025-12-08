70f708121a21e1ca7e5c7a8e5b2333fc388cda8d
SeungYeop-Han <ammezkhan@gmail.com>
2025-12-08 18:55:26 +0900

    feat(follow): Follow 엔티티 구현

--- src/main/java/com/gorogoro/followandlike/GorogoroFollowAndLikeApplication.java
+++ src/main/java/com/gorogoro/followandlike/GorogoroFollowAndLikeApplication.java
@@ -2,8 +2,10 @@ package com.gorogoro.followandlike;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GorogoroFollowAndLikeApplication {
     public static void main(String[] args) {
         SpringApplication.run(GorogoroFollowAndLikeApplication.class, args);

b19754521467a57be835e3ac8da0bf10934e51e8
SeungYeop-Han <ammezkhan@gmail.com>
2025-12-08 18:55:13 +0900

    feat(follow): Follow 도메인 예외 정의

---
+++ src/main/java/com/gorogoro/followandlike/follow/domain/exception/FollowException.java
@@ -0,0 +1,10 @@
+package com.gorogoro.followandlike.follow.domain.exception;
+
+import com.gorogoro.followandlike.common.exception.BaseDomainException;
+
+public class FollowException extends BaseDomainException {
+
+    public FollowException(String message) {
+        super(message);
+    }
+}

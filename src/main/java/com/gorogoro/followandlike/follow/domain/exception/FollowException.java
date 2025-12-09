9bf3047c73474763ef3fe1d3592c4dd173ad424b
SeungYeop-Han <ammezkhan@gmail.com>
2025-12-09 09:39:27 +0900

    feat(follow): 예외 사유를 별도의 enum 으로 분리

--- src/main/java/com/gorogoro/followandlike/follow/domain/exception/FollowException.java
+++ src/main/java/com/gorogoro/followandlike/follow/domain/exception/FollowException.java
@@ -1,10 +1,15 @@
 package com.gorogoro.followandlike.follow.domain.exception;
 
 import com.gorogoro.followandlike.common.exception.BaseDomainException;
+import lombok.Getter;
 
 public class FollowException extends BaseDomainException {
 
-    public FollowException(String message) {
-        super(message);
+    @Getter
+    private final FollowErrorCode code;
+
+    public FollowException(FollowErrorCode code) {
+        super(code.getMessage());
+        this.code = code;
     }
 }

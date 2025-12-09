c5612ea6a0885ed3b128422b9e46b88522fa528e
SeungYeop-Han <ammezkhan@gmail.com>
2025-12-09 15:41:39 +0900

    refactor(follow): Follow 엔티티 키 전략 변경
    기존 복합 기본키 방식에서 대리키 사용 + 유일키 도입 방식으로 변경
    
    #8

--- src/main/java/com/gorogoro/followandlike/follow/domain/exception/FollowErrorCode.java
+++ src/main/java/com/gorogoro/followandlike/follow/domain/exception/FollowErrorCode.java
@@ -6,12 +6,11 @@ import lombok.Getter;
 public enum FollowErrorCode {
 
     // CREATION_ERROR: 0xx
-    ID_IS_NULL("001", "id 가 null 입니다."),
-    FOLLOWER_ID_IS_NULL("002", "followerId 가 null 입니다."),
-    FOLLOWER_ID_IS_NEGATIVE("003", "followerId 가 음수입니다."),
-    FOLLOWEE_ID_IS_NULL("004", "followeeId 가 null 입니다."),
-    FOLLOWEE_ID_IS_NEGATIVE("005", "followeeId 가 음수입니다."),
-    FOLLOWED_ONESELF("006", "자기자신을 팔로우할 수 없습니다. followerId 와 followeeId 가 같습니다.");
+    FOLLOWER_ID_IS_NULL("001", "followerId 가 null 입니다."),
+    FOLLOWER_ID_IS_NEGATIVE("002", "followerId 가 음수입니다."),
+    FOLLOWEE_ID_IS_NULL("003", "followeeId 가 null 입니다."),
+    FOLLOWEE_ID_IS_NEGATIVE("004", "followeeId 가 음수입니다."),
+    FOLLOWED_ONESELF("005", "자기자신을 팔로우할 수 없습니다. followerId 와 followeeId 가 같습니다.");
 
     private final String code;
     private final String message;

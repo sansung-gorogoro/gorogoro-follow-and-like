b559a84fc5b686fa30bda899cfdb64b4dae992df
SeungYeop-Han <ammezkhan@gmail.com>
2025-12-09 16:37:24 +0900

    refactor(follow): 패키지명 변경 entity -> model
    #7

--- src/test/java/com/gorogoro/followandlike/follow/domain/model/FollowTest.java
+++ src/test/java/com/gorogoro/followandlike/follow/domain/model/FollowTest.java
@@ -1,4 +1,4 @@
-package com.gorogoro.followandlike.follow.domain.entity;
+package com.gorogoro.followandlike.follow.domain.model;
 
 import com.gorogoro.followandlike.follow.domain.exception.FollowException;
 import org.junit.jupiter.api.Test;

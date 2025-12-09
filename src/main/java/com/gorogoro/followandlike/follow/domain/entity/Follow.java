b8ec4e3258ca319dec8e049e95b90adf333ae0c3
SeungYeop-Han <ammezkhan@gmail.com>
2025-12-09 15:41:39 +0900

    refactor(follow): Follow 엔티티 키 전략 변경
    기존 복합 기본키 방식에서 대리키 사용 + 유일키 도입 방식으로 변경
    
    #8

--- src/main/java/com/gorogoro/followandlike/follow/domain/entity/Follow.java
+++ src/main/java/com/gorogoro/followandlike/follow/domain/entity/Follow.java
@@ -4,15 +4,16 @@ import com.gorogoro.followandlike.common.domain.entity.CreationAuditEntity;
 import com.gorogoro.followandlike.follow.domain.exception.FollowException;
 import jakarta.persistence.CheckConstraint;
 import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
-import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
 import java.util.Objects;
 
 import static com.gorogoro.followandlike.follow.domain.exception.FollowErrorCode.*;
@@ -20,20 +21,49 @@ import static com.gorogoro.followandlike.follow.domain.exception.FollowErrorCode
 @Entity
 @Table(
     name = "follow",
+    uniqueConstraints
+            = @UniqueConstraint(name = "uq_follower_id_followee_id", columnNames = {"follower_id", "followee_id"}),
     check = @CheckConstraint(name = "follower_must_different_from_followee", constraint = "follower_id <> followee_id")
 )
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Follow extends CreationAuditEntity {
 
-    @EmbeddedId
-    @Getter
-    private WhoFollowsWhom id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public Follow(WhoFollowsWhom id) {
-        if (id == null) {
-            throw new FollowException(ID_IS_NULL);
+    @Column(nullable = false)
+    @Getter
+    private Long followerId;
+
+    @Column(nullable = false)
+    @Getter
+    private Long followeeId;
+
+    public Follow(Long followerId, Long followeeId) {
+
+        validate(followerId, followeeId);
+
+        this.followerId = followerId;
+        this.followeeId = followeeId;
+    }
+
+    private void validate(Long followerId, Long followeeId) {
+        if (followerId == null) {
+            throw new FollowException(FOLLOWER_ID_IS_NULL);
         }
-        this.id = id;
+        if (followeeId == null) {
+            throw new FollowException(FOLLOWEE_ID_IS_NULL);
+        }
+        if (followerId < 0) {
+            throw new FollowException(FOLLOWER_ID_IS_NEGATIVE);
+        }
+        if (followeeId < 0) {
+            throw new FollowException(FOLLOWEE_ID_IS_NEGATIVE);
+        }
+        if (followerId.equals(followeeId)) {
+            throw new FollowException(FOLLOWED_ONESELF);
+        }
     }
 
     @Override
@@ -45,57 +75,5 @@ public class Follow extends CreationAuditEntity {
     @Override
     public int hashCode() {
         return Objects.hashCode(id);
-    }
-
-    /* ↓ 내부 키 클래스 ↓ */
-
-    @Embeddable
-    @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    public static class WhoFollowsWhom implements Serializable {
-
-        @Column(nullable = false)
-        @Getter
-        private Long followerId;
-
-        @Column(nullable = false)
-        @Getter
-        private Long followeeId;
-
-        public WhoFollowsWhom(Long followerId, Long followeeId) {
-
-            validate(followerId, followeeId);
-
-            this.followerId = followerId;
-            this.followeeId = followeeId;
-        }
-
-        private void validate(Long followerId, Long followeeId) {
-            if (followerId == null) {
-                throw new FollowException(FOLLOWER_ID_IS_NULL);
-            }
-            if (followeeId == null) {
-                throw new FollowException(FOLLOWEE_ID_IS_NULL);
-            }
-            if (followerId < 0) {
-                throw new FollowException(FOLLOWER_ID_IS_NEGATIVE);
-            }
-            if (followeeId < 0) {
-                throw new FollowException(FOLLOWEE_ID_IS_NEGATIVE);
-            }
-            if (followerId.equals(followeeId)) {
-                throw new FollowException(FOLLOWED_ONESELF);
-            }
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (!(o instanceof WhoFollowsWhom that)) return false;
-            return Objects.equals(followerId, that.followerId) && Objects.equals(followeeId, that.followeeId);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(followerId, followeeId);
-        }
     }
 }

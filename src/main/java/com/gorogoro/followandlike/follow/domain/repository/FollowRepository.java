e879ca658c166e7da80374870cb21cfb0723dd80
SeungYeop-Han <ammezkhan@gmail.com>
2025-12-10 18:32:47 +0900

    refactor(follow): lombok @NonNull 제거
    도메인 예외 던지는게 나은듯
    
    #13

--- src/main/java/com/gorogoro/followandlike/follow/domain/repository/FollowRepository.java
+++ src/main/java/com/gorogoro/followandlike/follow/domain/repository/FollowRepository.java
@@ -2,25 +2,24 @@ package com.gorogoro.followandlike.follow.domain.repository;
 
 import com.gorogoro.followandlike.follow.domain.model.Follow;
 import com.gorogoro.followandlike.follow.application.dto.CursorBasedPaginatedResult;
-import lombok.NonNull;
 
 import java.util.Optional;
 
 public interface FollowRepository {
 
-    Optional<Follow> findById(@NonNull Long id);
+    Optional<Follow> findById(Long id);
 
-    Optional<Follow> findByFollowerIdAndFolloweeId(@NonNull Long followerId, @NonNull Long followeeId);
+    Optional<Follow> findByFollowerIdAndFolloweeId(Long followerId, Long followeeId);
 
-    long countByFollowerId(@NonNull Long followerId);
+    long countByFollowerId(Long followerId);
 
-    long countByFolloweeId(@NonNull Long followeeId);
+    long countByFolloweeId(Long followeeId);
 
-    CursorBasedPaginatedResult<Follow> getFollowings(@NonNull Long followerId, Long pageCursor, int fetchSize);
+    CursorBasedPaginatedResult<Follow> getFollowings(Long followerId, Long pageCursor, int fetchSize);
 
-    CursorBasedPaginatedResult<Follow> getFollowers(@NonNull Long followeeId, Long pageCursor, int fetchSize);
+    CursorBasedPaginatedResult<Follow> getFollowers(Long followeeId, Long pageCursor, int fetchSize);
 
-    Follow save(@NonNull Follow follow);
+    Follow save(Follow follow);
 
-    void deleteByFollowerIdAndFolloweeId(@NonNull Long followerId, @NonNull Long followeeId);
+    void deleteByFollowerIdAndFolloweeId(Long followerId, Long followeeId);
 }
